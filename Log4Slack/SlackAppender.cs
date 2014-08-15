bdc74a08e798eefaccce85faf68d5898e8ba723c
Jonathan Freeland <jon.freeland@gmail.com>
2014-08-15 14:30:13 -0400

    Project created; NuGets added.

---
+++ Log4Slack/SlackAppender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Appender;
+
+namespace Log4Slack {
+    public class SlackAppender : AppenderSkeleton {
+        protected override void Append(log4net.Core.LoggingEvent loggingEvent) {
+            throw new NotImplementedException();
+        }
+    }
+}
